4dcfefcaa0aca8ce7a0fca882825e524f1fc7f8c
Devin Naquin <dnaquin@gmail.com>
2026-02-02 20:30:21 +0000

    exercise 03.23 zipWith

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -125,6 +125,9 @@ object List: // `List` companion object. Contains functions for creating and wor
     case (Nil, _) => Nil
     case (Cons(ah, at), Cons(bh, bt)) => Cons(ah + bh, addPairwise(at, bt))
 
-  // def zipWith - TODO determine signature
+  def zipWith[A, B, C](a: List[A], b: List[B], f: (A, B) => C): List[C] = (a, b) match
+    case (_, Nil) => Nil
+    case (Nil, _) => Nil
+    case (Cons(ah, at), Cons(bh, bt)) => Cons(f(ah, bh), zipWith(at, bt))
 
   def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = ???
