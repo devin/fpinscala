2cc23e278a597bf1ab1a85fe5ed42e21e5479f33
Devin Naquin <dnaquin@gmail.com>
2026-02-03 18:07:48 +0000

    exercise 04.03 map2

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -46,7 +46,10 @@ object Option:
     m => mean(xs.map(x => math.pow(x - m, 2)))
   )
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    (a, b) match
+      case (Some(a), Some(b)) => Some(f(a, b))
+      case _ => None
 
   def sequence[A](as: List[Option[A]]): Option[List[A]] = ???
 
