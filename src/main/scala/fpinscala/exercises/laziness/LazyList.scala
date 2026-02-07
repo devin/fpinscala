6453ab5da1c1c40ead550043e3e930e08981b38a
Devin Naquin <dnaquin@gmail.com>
2026-02-07 23:26:29 +0000

    exercise 05.05 takeWhile foldRight

--- src/main/scala/fpinscala/exercises/laziness/LazyList.scala
+++ src/main/scala/fpinscala/exercises/laziness/LazyList.scala
@@ -34,9 +34,12 @@ enum LazyList[+A]:
       case Empty => Empty
       case Cons(_, t) => t().drop(n-1)
 
-  def takeWhile(p: A => Boolean): LazyList[A] = this match
-    case Cons(h, t) if p(h()) => Cons(h, () => t().takeWhile(p))
-    case Empty => Empty
+  def takeWhile(p: A => Boolean): LazyList[A] =
+    foldRight(Empty: LazyList[A])((a, b) => if p(a) then
+      Cons(() => a, () => b)
+    else
+      Empty
+    )
 
   def forAll(p: A => Boolean): Boolean =
     foldRight(true)((a, b) => p(a) && b)
