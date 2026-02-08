4b6391f45dbc2033079d2fe29c5012d5615dc004
Devin Naquin <dnaquin@gmail.com>
2026-02-08 15:36:07 +0000

    exercise 05.09 from

--- src/main/scala/fpinscala/exercises/laziness/LazyList.scala
+++ src/main/scala/fpinscala/exercises/laziness/LazyList.scala
@@ -88,7 +88,7 @@ object LazyList:
 
   def continually[A](a: A): LazyList[A] = LazyList.cons(a, continually(a))
 
-  def from(n: Int): LazyList[Int] = ???
+  def from(n: Int): LazyList[Int] = LazyList.cons(n, from(n+1))
 
   lazy val fibs: LazyList[Int] = ???
 
