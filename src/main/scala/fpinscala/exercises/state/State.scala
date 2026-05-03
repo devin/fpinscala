a0047f10ed0b6d3f131d06cf84f8f11fb5f94fda
Devin Naquin <github@naqu.in>
2026-05-03 20:17:12 +0000

    exercise 06.05 double from map

--- src/main/scala/fpinscala/exercises/state/State.scala
+++ src/main/scala/fpinscala/exercises/state/State.scala
@@ -36,9 +36,8 @@ object RNG:
       // note. could be infinite loop.
       nonNegativeInt(rng2)
 
-  def double(rng: RNG): (Double, RNG) =
-    val (n, rng2) = nonNegativeInt(rng)
-    (n.toDouble / (Int.MaxValue.toDouble + 1), rng2)
+  def double: Rand[Double] =
+    map(nonNegativeInt)(n => n.toDouble / (Int.MaxValue.toDouble + 1))
 
   def intDouble(rng: RNG): ((Int,Double), RNG) =
     val (n, rng2) = rng.nextInt
